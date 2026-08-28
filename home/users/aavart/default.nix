8d48250a0519b08228eb43f015af1a5bf6a1eb12
Notorandxor_turing <rockstarfan0369@gmail.com>
2026-08-28 20:27:56 +0530

    [solved] dublication of same genrations and [added] nvim config

--- home/users/aavart/default.nix
+++ home/users/aavart/default.nix
@@ -1,4 +1,4 @@
-{ homeStateVersion, inputs, pkgs_unstable, ... }: {
+{ pkgs, homeStateVersion, inputs, pkgs_unstable, ... }: {
   imports = [
     inputs.nix4nvchad.homeManagerModules.default
     ../../modules/applications.nix
@@ -24,11 +24,69 @@
   programs.firefox.enable= true;
   programs.nvchad={
     enable= true;
+    extraConfig = ''
+      -- Custom vim options
+      vim.opt.shiftwidth = 2
+      vim.opt.tabstop = 2
+      vim.opt.expandtab = true
+      vim.wo.relativenumber = true
+  
+      -- Custom keymaps
+    '';
+    extraPackages = with pkgs; [
+      ripgrep
+      fd
+      fzf
+
+      # Lua
+      lua-language-server
+      stylua
+      
+      # Nix
+      nil
+      nixfmt-rfc-style
+
+      # Python
+      pyright 
+      black
+
+      # Rust
+      rust-analyzer
+      rustfmt
+
+      # C / C++
+      clang-tools
+
+      # C# 
+      csharpier
+
+      # JS 
+      typescript-language-server
+      prettier
+
+      # Markdown
+      marksman
+
+      # Bash
+      bash-language-server
+      shellcheck
+      shfmt
+      
+      # YAML / JSON
+      yaml-language-server
+
+      # Shell
+      bash-language-server
+    ];
   };
   programs.bash = {
     enable = true;
     shellAliases={
-      logseq= "logseqDB --ozone-platform=x11";
+      n = "nnn";
     };
+  };
+  programs.nnn ={
+    enable = true;
+    quitcd = true;
   };
 }
--- modules/desktop/plasma.nix
+++ modules/desktop/plasma.nix
@@ -1,9 +1,9 @@
 { pkgs, ... }: {
   services = {
     xserver.enable= true;
-    services.displayManager.sddm.enable = true;
-    services.displayManager.sddm.wayland.enable = true;
-    services.desktopManager.plasma6.enable = true;
+    displayManager.ly.enable = true;
+    # displayManager.ly.wayland.enable = true;
+    desktopManager.plasma6.enable = true;
   };
   # this create an additional genration
   #specialisation.plasma.configuration = {
